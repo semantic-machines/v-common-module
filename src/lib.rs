113a6f85c94f69e5f5f766b856baab32141bf55c
Itiua <ValeriyBushenev@gmail.com>
2021-01-22 11:52:09 +0300

    add fn get_storage_use_prop and remote_indv_r_storage.rs

--- src/lib.rs
+++ src/lib.rs
@@ -4,7 +4,11 @@ extern crate log;
 #[macro_use]
 extern crate scan_fmt;
 
+#[macro_use]
+extern crate lazy_static;
+
 pub mod info;
 pub mod module;
 pub mod onto;
 pub mod ticket;
+pub mod remote_indv_r_storage;
